469d0435640d057eae85f4c3b0f60b91e88a7d6e
Thridev Maharajh <maharajhthridev@gmail.com>
2026-05-29 22:51:49 +0200

    Created Quotation form.

---
+++ M2GiantGroupSystem/Quotation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M2GiantGroupSystem
+{
+    public partial class Quotation : Form
+    {
+        public Quotation()
+        {
+            InitializeComponent();
+        }
+    }
+}
