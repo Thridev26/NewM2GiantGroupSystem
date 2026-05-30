8d39459bf95c00a92644111a84fb551d4b4ec3d3
Thridev Maharajh <maharajhthridev@gmail.com>
2026-05-30 22:18:02 +0200

    Created an Inner join to display only In progress jobs on the Grid View.

--- M2GiantGroupSystem/AllocateAssetStafftoJob.cs
+++ M2GiantGroupSystem/AllocateAssetStafftoJob.cs
@@ -16,5 +16,34 @@ namespace M2GiantGroupSystem
         {
             InitializeComponent();
         }
+
+        private void jobBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.jobBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.groupWst1DataSet);
+
+        }
+
+        private void AllocateAssetStafftoJob_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'groupWst1DataSet.JobStaffAssignment' table. You can move, or remove it, as needed.
+            this.jobStaffAssignmentTableAdapter.Fill(this.groupWst1DataSet.JobStaffAssignment);
+            // TODO: This line of code loads data into the 'groupWst1DataSet.Staff' table. You can move, or remove it, as needed.
+            this.staffTableAdapter.Fill(this.groupWst1DataSet.Staff);
+            // TODO: This line of code loads data into the 'groupWst1DataSet.JobAssetAssignment' table. You can move, or remove it, as needed.
+            this.jobAssetAssignmentTableAdapter.Fill(this.groupWst1DataSet.JobAssetAssignment);
+            // TODO: This line of code loads data into the 'groupWst1DataSet.HiredAsset' table. You can move, or remove it, as needed.
+            this.hiredAssetTableAdapter.Fill(this.groupWst1DataSet.HiredAsset);
+            // TODO: This line of code loads data into the 'groupWst1DataSet.OwnedAsset' table. You can move, or remove it, as needed.
+            this.ownedAssetTableAdapter.Fill(this.groupWst1DataSet.OwnedAsset);
+            // Create an instance of the adapter containing your new query
+            GroupWst1DataSetTableAdapters.QueriesTableAdapter queries =
+                new GroupWst1DataSetTableAdapters.QueriesTableAdapter();
+
+            // Fill your DataGridView with the results
+            dataGridView1.DataSource = queries.GetDataByInProgress();
+
+        }
     }
 }
