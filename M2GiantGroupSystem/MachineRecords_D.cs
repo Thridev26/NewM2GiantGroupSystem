a1975735f8cec06a39deb2f857b7180c97647af3
Dharshan Govender (224097694) <224097694@stu.ukzn.ac.za>
2026-06-01 18:07:08 +0200

    coded clear fields button aligned all fields coded  search method coded cell click event handler

--- M2GiantGroupSystem/MachineRecords_D.cs
+++ M2GiantGroupSystem/MachineRecords_D.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,10 @@ namespace M2GiantGroupSystem
 
         private void MachineRecords_D_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'groupWst1DataSet.HiredAsset' table. You can move, or remove it, as needed.
+            this.hiredAssetTableAdapter.Fill(this.groupWst1DataSet.HiredAsset);
+            // TODO: This line of code loads data into the 'groupWst1DataSet.OwnedAsset' table. You can move, or remove it, as needed.
+            this.ownedAssetTableAdapter.Fill(this.groupWst1DataSet.OwnedAsset);
             tabControl1.SelectedIndex = TabIndex;
             tabControl1.DrawMode = TabDrawMode.OwnerDrawFixed;
             tabControl1.DrawItem += tabControl1_DrawItem;
@@ -72,50 +77,95 @@ namespace M2GiantGroupSystem
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            // adds row from datagridview to textboxes and comboboxes for owned asset
+             
             {
                 if (e.RowIndex < 0) return;
 
-                //DataGridViewRow row = dataGridView2.Rows[e.RowIndex];
+                DataGridViewRow row = dgvOwnedAsset_D.Rows[e.RowIndex];
 
-                //    textBox2.Text = row.Cells[0].Value?.ToString() ?? "";
-                //    textBox3.Text = row.Cells[1].Value?.ToString() ?? "";
-                //    comboBox3.Text = row.Cells[2].Value?.ToString() ?? "";
+                   txtAssetID_D.Text = row.Cells[0].Value?.ToString() ?? "";
 
-                //    dateTimePicker2.Value = Convert.ToDateTime(row.Cells[3].Value);
+                   txtAsSnumber_d.Text = row.Cells[1].Value?.ToString() ?? "";
 
-                //    comboBox2.Text = row.Cells[4].Value?.ToString() ?? "";
+                    cmbType_D.Text = row.Cells[2].Value?.ToString() ?? "";
 
-                //    dtServicedt.Value = Convert.ToDateTime(row.Cells[5].Value);
+                  dtPurchaseDate_D.Value = Convert.ToDateTime(row.Cells[3].Value);
 
-                //    comboBox1.Text = row.Cells[6].Value?.ToString() ?? "";
-                //}
+                    cmbCondit_D.Text = row.Cells[4].Value?.ToString() ?? "";
+
+                    dtServiceDate_D.Value = Convert.ToDateTime(row.Cells[5].Value);
+
+                    cmbStatus_D.Text = row.Cells[6].Value?.ToString() ?? "";
+                
             }
         }
 
         private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
         {
-            //DataGridViewRow row = dataGridView2.Rows[e.RowIndex];
-            //string status = row.Cells[6].Value?.ToString() ?? "";
+            DataGridViewRow row = dgvOwnedAsset_D.Rows[e.RowIndex];
+            string status = row.Cells[6].Value?.ToString() ?? "";
 
-            //switch (status)
-            //{
-            //    case "Available":
-            //        row.Cells[6].Style.BackColor = Color.LightGreen;
-            //        row.Cells[6].Style.ForeColor = Color.Black;
-            //        break;
-            //    case "In Use":
-            //        row.Cells[6].Style.BackColor = Color.LightCoral;
-            //        row.Cells[6].Style.ForeColor = Color.Black;
-            //        break;
-            //    case "Under Maintenance":
-            //        row.Cells[6].Style.BackColor = Color.LightYellow;
-            //        row.Cells[6].Style.ForeColor = Color.Black;
-            //        break;
-            //    default:
-            //        row.Cells[6].Style.BackColor = Color.White;
-            //        row.Cells[6].Style.ForeColor = Color.Black;
-            //        break;
+            switch (status)
+            {
+                case "Available":
+                    row.Cells[6].Style.BackColor = Color.LightGreen;
+                    row.Cells[6].Style.ForeColor = Color.Black;
+                    break;
+                case "In Use":
+                    row.Cells[6].Style.BackColor = Color.LightCoral;
+                    row.Cells[6].Style.ForeColor = Color.Black;
+                    break;
+                case "Under Maintenance":
+                    row.Cells[6].Style.BackColor = Color.LightYellow;
+                    row.Cells[6].Style.ForeColor = Color.Black;
+                    break;
+                default:
+                    row.Cells[6].Style.BackColor = Color.White;
+                    row.Cells[6].Style.ForeColor = Color.Black;
+                    break;
+
+            }
+        }
+
+        private void txtSearchOA_D_TextChanged(object sender, EventArgs e)
+        {
+            ownedAssetTableAdapter.FillBySerialNumber(groupWst1DataSet.OwnedAsset, txtSearchOA_D.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to clear all fields?", "Confirm Clear", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                txtAssetID_D.Text = "";
+
+                txtAsSnumber_d.Text = "";
+
+                cmbType_D.SelectedIndex = -1;
+
+                cmbType_D.Text = "";
+
+                dtPurchaseDate_D.Value = DateTime.Now;
+
+                cmbCondit_D.SelectedIndex = -1;
+                cmbCondit_D.Text = "";
+
+                dtServiceDate_D.Value = DateTime.Now;
+
+                cmbStatus_D.SelectedIndex = -1;
+                cmbStatus_D.Text = "";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAssetID_D_TextChanged(object sender, EventArgs e)
+        {
 
         }
     } 
