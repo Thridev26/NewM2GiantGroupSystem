b49cd6d18b9408df7b6d1b1339404cb7785ceafc
Thridev Maharajh <maharajhthridev@gmail.com>
2026-06-02 09:52:29 +0200

    Created real hashes for existing staff members using BCrypt.Net

--- M2GiantGroupSystem/LoginForm.cs
+++ M2GiantGroupSystem/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using Microsoft.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -52,5 +53,27 @@ namespace M2GiantGroupSystem
                 passwordBox.UseSystemPasswordChar = true;
             }
         }
+
+        //This method is for demonstration purposes only. It shows how to hash a password and update it in the database.
+        //private void button3_Click(object sender, EventArgs e)
+        //{
+        //    // 1. Generate the secure hash for an existing password
+        //    string realPassword = "branch16"; // The password you want the user to have
+        //    string secureHash = BCrypt.Net.BCrypt.HashPassword(realPassword);
+
+        //    // 2. Update your database with this new, long, secure string
+        //    // Replace '1' with the specific staffID you are updating
+        //    string updateQuery = "UPDATE Staff SET passwordHash = @hash WHERE staffID = 5";
+
+        //    using (SqlConnection conn = new SqlConnection("Data Source=146.230.177.46;Initial Catalog=GroupWst1;Persist Security Info=True;User ID=GroupWst1;Password=dtf39;Encrypt=True;Trust Server Certificate=True"))
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = new SqlCommand(updateQuery, conn))
+        //        {
+        //            cmd.Parameters.AddWithValue("@hash", secureHash);
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
     }
 }
