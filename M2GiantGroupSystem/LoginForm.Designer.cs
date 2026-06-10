be9bd092230fab2eaae3d56af432054106d5a9ef
Thridev Maharajh <maharajhthridev@gmail.com>
2026-06-10 23:56:58 +0200

    Added dharshans theme.

--- M2GiantGroupSystem/LoginForm.Designer.cs
+++ M2GiantGroupSystem/LoginForm.Designer.cs
@@ -222,6 +222,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
