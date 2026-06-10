0b632150c3e693871d1c23181d056a16b8a445a9
Thridev Maharajh <maharajhthridev@gmail.com>
2026-06-10 23:41:42 +0200

    Added Ashmikas design and Dharshan's themes.

--- M2GiantGroupSystem/Quotation.Designer.cs
+++ M2GiantGroupSystem/Quotation.Designer.cs
@@ -1643,6 +1643,7 @@
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Quotation";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quotation";
