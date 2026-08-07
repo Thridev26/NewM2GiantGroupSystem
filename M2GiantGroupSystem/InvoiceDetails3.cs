c548043c5525994506850bc5e99aea2497f2e40a
Ash2218 <ashmikaramsaran34@gmail.com>
2026-08-07 14:55:48 +0200

    fixed invoicing

--- M2GiantGroupSystem/InvoiceDetails3.cs
+++ M2GiantGroupSystem/InvoiceDetails3.cs
@@ -12,7 +12,7 @@ namespace M2GiantGroupSystem
         public string ClientName { get; set; }
         public string SiteAddress { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+      //  public DateTime EndDate { get; set; }
         public string JobTypeName { get; set; }
         public decimal JobRate { get; set; }
         public decimal DetailValue { get; set; }
