67f48a5e6ee34d4fe12b130b100ea0bdf853db9c
Ash2218 <ashmikaramsaran34@gmail.com>
2026-08-29 22:33:03 +0200

    format edits

--- M2GiantGroupSystem/profitReportForm.Designer.cs
+++ M2GiantGroupSystem/profitReportForm.Designer.cs
@@ -51,20 +51,20 @@
             // dt1
             // 
             this.dt1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dt1.Location = new System.Drawing.Point(712, 241);
+            this.dt1.Location = new System.Drawing.Point(607, 194);
             this.dt1.Margin = new System.Windows.Forms.Padding(6, 10, 6, 10);
             this.dt1.Name = "dt1";
-            this.dt1.Size = new System.Drawing.Size(813, 38);
+            this.dt1.Size = new System.Drawing.Size(608, 38);
             this.dt1.TabIndex = 1;
             this.dt1.ValueChanged += new System.EventHandler(this.dt1_ValueChanged);
             // 
             // dt2
             // 
             this.dt2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dt2.Location = new System.Drawing.Point(699, 335);
+            this.dt2.Location = new System.Drawing.Point(607, 339);
             this.dt2.Margin = new System.Windows.Forms.Padding(6, 10, 6, 10);
             this.dt2.Name = "dt2";
-            this.dt2.Size = new System.Drawing.Size(813, 38);
+            this.dt2.Size = new System.Drawing.Size(608, 38);
             this.dt2.TabIndex = 0;
             this.dt2.ValueChanged += new System.EventHandler(this.dt2_ValueChanged);
             // 
@@ -105,7 +105,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(285, 250);
+            this.label2.Location = new System.Drawing.Point(213, 337);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(242, 41);
@@ -117,7 +117,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(272, 124);
+            this.label1.Location = new System.Drawing.Point(213, 182);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(255, 41);
@@ -128,7 +128,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(825, 136);
+            this.label3.Location = new System.Drawing.Point(605, 75);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(602, 50);
@@ -165,12 +165,12 @@
             this.button2.BackColor = System.Drawing.Color.DarkGreen;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(807, 550);
+            this.button2.Location = new System.Drawing.Point(607, 489);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(519, 151);
+            this.button2.Size = new System.Drawing.Size(608, 151);
             this.button2.TabIndex = 50;
-            this.button2.Text = "Generate expense report";
+            this.button2.Text = "Generate profit report";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -194,13 +194,13 @@
             this.crystalReportViewer1.Location = new System.Drawing.Point(6, 10);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
             this.crystalReportViewer1.ReportSource = this.WeeklyProfitCrystalReport1;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(1904, 991);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1904, 1006);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // profitReportForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.SeaShell;
             this.ClientSize = new System.Drawing.Size(1924, 1055);
             this.Controls.Add(this.tabControl1);
